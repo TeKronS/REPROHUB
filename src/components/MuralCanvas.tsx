6d80ca931b27aa30e35e5e3232a2125a7f96fb8c
Simon Enrique Ramirez Ferrer <Asper.Tk93@gmail.com>
2026-05-22 23:21:19 +0000

    Try fixing this error: `Runtime ReferenceError: Separator is not defined

--- src/components/MuralCanvas.tsx
+++ src/components/MuralCanvas.tsx
@@ -3,6 +3,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
 
 interface MuralCanvasProps {
   imageUrl: string | null;
